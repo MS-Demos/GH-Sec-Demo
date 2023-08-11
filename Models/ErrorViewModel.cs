458a84fbe5089e8915defec5ee73dae3abbef9b5
GerardoRamosCol <lgerardoramos@hotmail.com>
2023-08-11 11:29:08 -0700

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace gh_adv_sec_demo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
